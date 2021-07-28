caacb2b133a3865bd25c3d287f80c9fb5f4283f4
Josip Antoliš <josip.antolis@protonmail.com>
2021-07-28 23:09:24 +0200

    Fix issue with doc files and flatpak build
    Add templating support to gschema file and use it to adapt homepage key's default value for both raw ninja and flatpak builds

--- meson/post_install.py
+++ meson/post_install.py
@@ -2,11 +2,27 @@
 
 import os
 import subprocess
+import sys
 
-schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
-mimedir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'mime')
+project_name = sys.argv[1]
+prefix = os.environ['MESON_INSTALL_PREFIX']
+schemadir = os.path.join(prefix, 'share', 'glib-2.0', 'schemas')
+schema_file = os.path.join(schemadir, project_name + '.gschema.xml')
+mimedir = os.path.join(prefix, 'share', 'mime')
+
+def fill_schema_placeholders():
+    with open(schema_file, 'rt') as file:
+        schema_content = file.read()
+    for param in os.environ.keys():
+        key = '{{' + param + '}}'
+        value = os.environ[param]
+        schema_content = schema_content.replace(key, value)
+    with open(schema_file, 'wt') as file:
+        file.write(schema_content)
 
 if not os.environ.get('DESTDIR'):
+    print('Replacing gsettings placeholders in ' + schema_file + '...')
+    fill_schema_placeholders()
     print('Compiling gsettings schemas...')
     subprocess.call(['glib-compile-schemas', schemadir])
     print('Registering text/gemini MIME type...')
